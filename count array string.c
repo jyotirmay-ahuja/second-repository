85167313fef8984830214a42312f3869924de27e
jyotirmay-ahuja <125427670+jyotirmay-ahuja@users.noreply.github.com>
2023-03-15 10:09:53 +0530

    Add files via upload

---
+++ count array string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char str[20];
+int i=0;
+printf("\nEnter the string;");
+gets (str);
+while(str[i]!='\0')
+i++;
+printf("\nLength of [%s] is [%d]\n",str,i);
+return 0;
+}
